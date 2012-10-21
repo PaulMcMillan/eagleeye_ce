aa4f65e1b24498981bd38c9afe03cd1565b12d54
Paul McMillan <paul.mcmillan@nebula.com>
2012-10-20 21:14:47 -0400

    Move things to config files

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,4 +1,8 @@
-
+# You might want to modify this stuff
+BROKER_URL = 'amqp://'
+CELERYD_CONCURRENCY = 10
+CELERYD_TASK_SOFT_TIME_LIMIT = 30
+CELERYD_TASK_TIME_LIMIT = 45
 
 # You probably don't want to modify the stuff below this line
 CELERYD_PREFETCH_MULTIPLIER = 1
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 celery purge
 celery call eagleeye_ce.tasks.get_shodan_results
-celery -A eagleeye_ce.tasks worker --loglevel=info --soft-time-limit=30 --time-limit=45 --concurrency=3
+celery -A eagleeye_ce.tasks worker --loglevel=info

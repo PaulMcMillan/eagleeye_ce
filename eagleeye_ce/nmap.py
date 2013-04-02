41e60bc90c2f7b3299ebf592dc2e7e0cb6961163
Paul McMillan <paul.mcmillan@nebula.com>
2013-04-02 18:33:19 -0400

    stupid rm *. get nmap back in shape

--- eagleeye_ce/nmap.py
+++ eagleeye_ce/nmap.py
@@ -7,26 +7,31 @@ from eagleeye_ce import utils
 
 
 @celery.task
-def verify_open(hosts, ports):
-    """ Basic quick nmap open port command. Returns a list of open
-    service,host,port tuples.
-    """
-    hosts, ports = utils.iterit(hosts, ports, cast=str)
+def filter_open(hosts):
+    """ Basic quick nmap open port command. Filters a list of
+    shodan-style hosts/port results to those that are open.
 
-    command = ['nmap', '-T5', '--no-stylesheet', '--open', '-Pn', '-sT',
-               '--min-rate', '500', '--host-timeout', '5s',
+    This is inefficient, but n ~= 100 so it doesn't matter much.
+    """
+    host_list = list(set(h['ip'] for h in hosts))
+    port_list = set(str(h['port']) for h in hosts)
+
+    command = ['nmap', '-T5', '--no-stylesheet', '-Pn', '-sT',
+               '--min-rate', '500', '--host-timeout', '15s',
                '-oX', '-',  # output XML to stdout
-               '-p', ','.join(ports)] + hosts
+               '-p', ','.join(port_list)] + host_list
     nmap_xml_output = subprocess.check_output(command)
     nmaprun = objectify.fromstring(nmap_xml_output)
 
-    result = []
-    for host in nmaprun.host[:]:
+    checked = {}
+    for host in nmaprun.host:
         for port in host.ports.port:
-            item = (port.service.get('name'),
-                    host.address.get('addr'),
-                    port.get('portid'))
-            result.append(item)
-    print result
+            state = port.state.get('state')
+            checked[host.address.get('addr'), port.get('portid')] = state
+
+    result = []
+    for host in hosts:
+        if checked[host['ip'], str(host['port'])] == 'open':
+            result.append(host)
     return result
 
--- provision_script.sh
+++ provision_script.sh
@@ -4,9 +4,11 @@ sudo apt-get install git unzip xvfb rabbitmq-server nmap chromium-browser -y
 #sudo apt-get install build-essential python-dev -y # optional for librabbitmq
 curl http://python-distribute.org/distribute_setup.py | sudo python
 curl https://raw.github.com/pypa/pip/master/contrib/get-pip.py | sudo python
-sudo pip install celery selenium pyvirtualdisplay shodan
+
 #sudo pip install librabbitmq # optional for queue performance
 curl http://chromedriver.googlecode.com/files/chromedriver_linux64_23.0.1240.0.zip | funzip > chromedriver
 chmod +x chromedriver
 sudo mv chromedriver /usr/bin/chromedriver
 git clone git@github.com:PaulMcMillan/eagleeye_ce.git
+cd eagleeye_cd
+sudo pip install -U -r requirements.txt
